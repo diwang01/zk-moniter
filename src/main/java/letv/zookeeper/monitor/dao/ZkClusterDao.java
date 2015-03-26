9f3f741214e6a49f0c1e004a76fcf928ef92c094
王迪 <wangdi0410@gmail.com>
2015-03-26 11:29:53 +0800

    add

---
+++ src/main/java/letv/zookeeper/monitor/dao/ZkClusterDao.java
@@ -0,0 +1,16 @@
+package letv.zookeeper.monitor.dao;
+
+import letv.zookeeper.monitor.model.ZkClusterInfo;
+
+/**
+ * zookeeper集群信息dao
+ * @author shaoxiangfei
+ *
+ */
+public interface ZkClusterDao {
+	public ZkClusterInfo getClusterInfo();
+	
+	public void updateClusterInfo(ZkClusterInfo zkClusterInfo);
+	
+	public int insertClusterInfo(ZkClusterInfo zkClusterInfo);
+}
---
+++ src/main/java/letv/zookeeper/monitor/exception/SSHException.java
@@ -0,0 +1,23 @@
+package letv.zookeeper.monitor.exception;
+
+/**
+ * SShException
+ * @author shaoxiangfei
+ *
+ */
+public class SSHException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+	
+	public SSHException() {
+		super();
+	}
+	
+	public SSHException(String message) {
+		super(message);
+	}
+	
+	public SSHException(String message, Throwable throwable) {
+		super(message, throwable);
+	}
+}
